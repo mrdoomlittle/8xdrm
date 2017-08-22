242ec2b1f9c95817e90d6058f887476ddbe060ee
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-08-22 14:08:08 +0100

    .

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
-gcc -c -std=c11 -o 8xdrm.o 8xdrm.c
+gcc -c -std=c11 $1 -o 8xdrm.o 8xdrm.c
 ar rc lib/lib8xdrm.a 8xdrm.o
 cp 8xdrm.h inc
 
 
-gcc -std=c11 -o main main.c 8xdrm.o
+gcc -std=c11 $1-o main main.c 8xdrm.o
